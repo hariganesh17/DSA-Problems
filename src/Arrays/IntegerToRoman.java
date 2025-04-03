184d5a4c9b2073ba829f4ff4437e85733fc589d8
hariganesh <hariganeshan17@gmail.com>
2025-04-04 01:23:08 +0530

    Integer to Roman

---
+++ src/Arrays/IntegerToRoman.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class IntegerToRoman {
+    public static String convert(int num){
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols ={"M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            while(num >= values[i]){
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String[] args){
+        int num = 1999;
+        System.out.println(convert(num));
+    }
+}
