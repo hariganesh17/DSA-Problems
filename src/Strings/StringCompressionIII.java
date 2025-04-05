055ad90655b72b5d8a7b47f760ea8d3d78fc62af
hariganesh <hariganeshan17@gmail.com>
2025-04-05 12:52:36 +0530

    String compression type3

--- src/Strings/StringCompressionIII.java
+++ src/Strings/StringCompressionIII.java
@@ -1,4 +1,24 @@
 package Strings;
 
 public class StringCompressionIII {
+    public static String compress(String s){
+        StringBuilder comp = new StringBuilder();
+        int i =0;
+
+        while(i<s.length()){
+            char currentChar = s.charAt(i);
+            int count = 0;
+
+            while(i < s.length() && s.charAt(i) == currentChar && count < 9){
+                i++;
+                count++;
+            }
+            comp.append(currentChar).append(count);
+        }
+        return comp.toString();
+    }
+    public static void main(String[] args){
+        String s = "aaaaaaaaaaaaaabb";
+        System.out.println(compress(s));
+    }
 }
