bfb958e984bdafe3c16956a3f03a3869b54d1f19
hariganesh <hariganeshan17@gmail.com>
2025-04-06 10:17:41 +0530

    Climbing stairs / fibonacci

--- src/NumberOperation/Fibonacci.java
+++ src/NumberOperation/Fibonacci.java
@@ -15,8 +15,23 @@ public class Fibonacci {
         }
         return b;
     }
+
+    //Climbing stairs
+    public static int climb(int n){
+        if(n < 2) return n;
+        int first = 1;
+        int second = 2;
+
+        for(int i=3;i<=n;i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
     public static void main(String[] args) {
-        int n = 9;
-        System.out.println(n + "th Fibonacci number is: " + fibonacci(n));
+        int n = 4;
+       // System.out.println(n + "th Fibonacci number is: " + fibonacci(n));
+        System.out.println(climb(n));
     }
 }
