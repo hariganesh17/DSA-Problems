3c99ee5541e0f34ac45b67f7eb79ddbf8522e498
hariganesh <hariganeshan17@gmail.com>
2025-04-06 18:22:23 +0530

    find the square root

--- src/NumberOperation/SquareRoot.java
+++ src/NumberOperation/SquareRoot.java
@@ -1,4 +1,27 @@
 package NumberOperation;
 
 public class SquareRoot {
+    public static int find(int n){
+        if( n <2) return 0;
+
+        int left = 1;
+        int right = n/2;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            long square =(long) mid * mid;
+
+            if(square == n) return mid;
+
+            else if(square < n) left = mid+1;
+
+            else right = mid-1;
+        }
+        return right;
+    }
+    public static void main(String[] args){
+        int n = 3;
+        System.out.println(find(16));
+    }
 }
