aceefa9117f2f2d5cace2e75ef7ca1dd54d6caa0
hariganesh <hariganeshan17@gmail.com>
2025-04-07 22:26:08 +0530

    validate BST

--- src/BinarySerachTree/ValidBST.java
+++ src/BinarySerachTree/ValidBST.java
@@ -4,6 +4,7 @@ public class ValidBST {
     public static boolean validBST(Node root){
         return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
+    //fix the range , ie: if at node then it should be b/w min and max value
     public static boolean isValidBST(Node root,long minValue, long maxValue){
         if(root == null) return true;
         if(root.data >= maxValue || root.data <= minValue) return false;
