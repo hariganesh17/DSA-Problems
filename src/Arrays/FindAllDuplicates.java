6f2c34183858f1ff120d09de49457bbb474468d3
hariganesh <hariganeshan17@gmail.com>
2025-04-07 22:23:23 +0530

    find all duplicates in array

--- src/Arrays/FindAllDuplicates.java
+++ src/Arrays/FindAllDuplicates.java
@@ -1,4 +1,25 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class FindAllDuplicates {
+    //Brute force
+    public static List<Integer> find(int[] arr){
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+
+        for(int num : arr){
+            if(!seen.add(num)){
+                duplicates.add(num);
+            }
+        }
+        return new ArrayList<>(duplicates);
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,2,4,5,5,4};
+        System.out.println(find(arr));
+    }
 }
