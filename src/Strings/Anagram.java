f3245508e7ece8c98044ae7ea115411b72352624
hariganesh <hariganeshan17@gmail.com>
2025-04-08 02:11:00 +0530

    Anagram update

--- src/Strings/Anagram.java
+++ src/Strings/Anagram.java
@@ -1,6 +1,12 @@
 package Strings;
 
 public class Anagram {
+   /* An anagram is a word or phrase formed by rearranging the letters of another.
+    For two strings to be anagrams:
+    They must have exactly the same characters.
+    The frequency of each character must also be the same.
+    Order doesn't matter.*/
+
     public static boolean isValid(String s,String t){
         if(s.length() != t.length()) return false;
 
